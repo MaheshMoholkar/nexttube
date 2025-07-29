03d7f40d5ad6fa80d9de2ebc1625bd553351a80c
Mahesh Moholkar <maheshmoholkar171@gmail.com>
2025-07-29 13:49:57 +0530

    add videos section

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import { useRouter } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -23,14 +32,50 @@ export const VideosSectionSuspense = () => {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     }
   );
+
+  const router = useRouter();
+
   return (
-    <>
-      {JSON.stringify(data)}
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Likes</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <TableRow
+                  className="cursor-pointer"
+                  key={video.id}
+                  onClick={() => router.push(`/studio/videos/${video.id}`)}
+                >
+                  <TableCell>{video.title}</TableCell>
+                  <TableCell>visibility</TableCell>
+                  <TableCell>status</TableCell>
+                  <TableCell>date</TableCell>
+                  <TableCell>views</TableCell>
+                  <TableCell>likes</TableCell>
+                  <TableCell>comments</TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
       <InfiniteScroll
         hasNextPage={query.hasNextPage}
         isFetchingNextPage={query.isFetchingNextPage}
         fetchNextPage={query.fetchNextPage}
       />
-    </>
+    </div>
   );
 };
