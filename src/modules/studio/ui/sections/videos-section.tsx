00e10c0e1ec61c32a33069ec5536eaab147cbe9d
Mahesh Moholkar <maheshmoholkar171@gmail.com>
2025-08-01 11:25:52 +0530

    add table skeleton

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -17,14 +17,68 @@ import { ErrorBoundary } from "react-error-boundary";
 import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
 import { snakeCaseToTitle } from "@/lib/utils";
 import { format } from "date-fns";
+import { Globe2Icon, LockIcon } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const VideosSection = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<VideoSectionSkeleton />}>
       <ErrorBoundary fallback={<div>Error</div>}>
         <VideosSectionSuspense />
       </ErrorBoundary>
     </Suspense>
+  );
+};
+
+const VideoSectionSkeleton = () => {
+  return (
+    <>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Likes</TableHead>
+              <TableHead className="text-right pr-6">Comments</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <TableRow key={index}>
+                <TableCell className="pl-6">
+                  <div className="flex items-center gap-4">
+                    <Skeleton className="h-20 w-36" />
+                    <div className="flex flex-col gap-2">
+                      <Skeleton className="h-4 w-[70px]" />
+                      <Skeleton className="h-3 w-[100px]" />
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-20" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-20" />
+                </TableCell>
+                <TableCell>
+                  <Skeleton className="h-4 w-20" />
+                </TableCell>
+                <TableCell className="text-right">
+                  <Skeleton className="h-4 w-20 ml-auto" />
+                </TableCell>
+                <TableCell className="text-right pr-6">
+                  <Skeleton className="h-4 w-20 ml-auto" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 };
 
@@ -50,7 +104,7 @@ export const VideosSectionSuspense = () => {
               <TableHead>Date</TableHead>
               <TableHead className="text-right">Views</TableHead>
               <TableHead className="text-right">Likes</TableHead>
-              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Comments</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
@@ -62,7 +116,7 @@ export const VideosSectionSuspense = () => {
                   key={video.id}
                   onClick={() => router.push(`/studio/videos/${video.id}`)}
                 >
-                  <TableCell>
+                  <TableCell className="pl-6">
                     <div className="flex items-center gap-4">
                       <div className="relative aspect-video w-36 shrink-0">
                         <VideoThumbnail
@@ -82,7 +136,16 @@ export const VideosSectionSuspense = () => {
                       </div>
                     </div>
                   </TableCell>
-                  <TableCell>{snakeCaseToTitle(video.visibility)}</TableCell>
+                  <TableCell>
+                    <div className="flex items-center">
+                      {video.visibility === "private" ? (
+                        <LockIcon className="size-4 mr-2" />
+                      ) : (
+                        <Globe2Icon className="size-4 mr-2" />
+                      )}
+                      {snakeCaseToTitle(video.visibility)}
+                    </div>
+                  </TableCell>
                   <TableCell>
                     <div className="flex items-center">
                       {snakeCaseToTitle(video.muxStatus || "pending")}
@@ -91,9 +154,11 @@ export const VideosSectionSuspense = () => {
                   <TableCell>
                     {format(new Date(video.createdAt), "MMM d yyyy")}
                   </TableCell>
-                  <TableCell>views</TableCell>
-                  <TableCell>likes</TableCell>
-                  <TableCell>comments</TableCell>
+                  <TableCell className="text-right text-sm">views</TableCell>
+                  <TableCell className="text-right text-sm">likes</TableCell>
+                  <TableCell className="text-right text-sm pr-6">
+                    comments
+                  </TableCell>
                 </TableRow>
               ))}
           </TableBody>
